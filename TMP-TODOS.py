f56cb0a385bc90e41de8c56e40016772ff8f9b78
Paweł Brzozowski <mac@MacBook-Air-Pawe.local>
2021-11-03 14:51:27 +0100

    added TODOs
    for now in tmp python file

---
+++ TMP-TODOS.py
@@ -0,0 +1,9 @@
+# TODO - show photos, for example show every 10th photo.
+
+# TODO - save cords of opposite corners (top left, bottom right)
+
+# TODO - draw rectagle wtih mouse clicks on every 10th photo
+
+# TODO - calculate the displacement of the center of the marked area in the example of 10 photos
+
+# TODO - Calculate the changes in the size of the selected area between, for example, frame no. 0 and frame no. 10
