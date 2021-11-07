61045d364492017a509176e31e738be19c6c8035
Paweł Brzozowski <mac@192.168.1.5>
2021-11-07 11:36:05 +0100

    beta version of drawning

--- drawning_test.py
+++ drawning_test.py
@@ -1,30 +1,9 @@
 import cv2
 import numpy as np
 
-
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle.
 ix,iy = -1,-1
-current_img = ""
-
-
-# This functions is responsible for showing every <number> photo.
-# Please press any key to show next photo.
-def show_photo(list_of_paths, photos_skipped = 5):
-    anotated_photos = []
-    file_index = 0
-    for file_path  in list_of_paths:
-        if file_index == 0:
-            img = cv2.imread(file_path)
-            cv2.imshow(file_path, img)
-            cv2.waitKey(0)  # waits until a key is pressed
-            cv2.destroyAllWindows()  # destroys the window showing image
-            anotated_photos.append(file_path)
-        print(f"current index number = {file_index} - file: {file_path}")
-        file_index = file_index + 1
-        if file_index >= photos_skipped:
-            file_index = 0
-        return anotated_photos
 
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import drawning_test
 import path_finder
 import show_photos
-
 
 # Getting path for everything file in particular photos directory.
 # This method required path (String type) as parameter in other case it's use default.
@@ -10,4 +10,7 @@ print(path_list)
 # TODO check what in case that there will be more different folders with photos instead just 1.
 # Showing very <photos_skipped> photo.
 # Please specify <list_of_paths> as list that you have gotten from get_path_for_files()
-anotated_photos = show_photos.show_photo(list_of_paths=path_list, photos_skipped=5)
+
+# anotated_photos = show_photos.show_photo(list_of_paths=path_list, photos_skipped=5)
+
+drawning_test.draw_circle()
