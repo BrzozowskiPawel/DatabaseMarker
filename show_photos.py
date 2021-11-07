6d05712932f137b64ecec70759184c9c1e81d25d
Paweł Brzozowski <mac@192.168.1.5>
2021-11-07 11:19:29 +0100

    return list of anotated photos

--- show_photos.py
+++ show_photos.py
@@ -3,6 +3,7 @@ import cv2
 # This functions is responsible for showing every <number> photo.
 # Please press any key to show next photo.
 def show_photo(list_of_paths, photos_skipped = 5):
+    anotated_photos = []
     file_index = 0
     for file_path  in list_of_paths:
         if file_index == 0:
@@ -10,7 +11,9 @@ def show_photo(list_of_paths, photos_skipped = 5):
             cv2.imshow(file_path, img)
             cv2.waitKey(0)  # waits until a key is pressed
             cv2.destroyAllWindows()  # destroys the window showing image
-        # print(f"current index number = {file_index} - file: {file_path}")
+            anotated_photos.append(file_path)
+        print(f"current index number = {file_index} - file: {file_path}")
         file_index = file_index + 1
         if file_index >= photos_skipped:
             file_index = 0
+        return anotated_photos
