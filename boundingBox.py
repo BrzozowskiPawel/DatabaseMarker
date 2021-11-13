7575ca695d1ccb764de4742344bb80909cd49c62
fabianlewandowski <farba98@gmail.com>
2021-11-13 16:40:35 +0100

    Tutaj działa oznaczanie i usuwanie oznaczeń !!!!

--- boundingBox.py
+++ boundingBox.py
@@ -2,10 +2,10 @@ import cv2
 
 
 class BoundingBox:
-    def __init__(self, name,x,y,dx,dy):
-        self.name = name
+    def __init__(self, path,x_,y_,x,y):
+        self.path = path
+        self.x_ = x_
+        self.y_ = y_
         self.x = x
         self.y = y
-        self.dx = dx
-        self.dy = dy
 
--- show_photos.py
+++ show_photos.py
@@ -1,52 +1,75 @@
 import cv2
+from dataclasses import dataclass
 from boundingBox import BoundingBox
 
 bounding_box_list = []
-x_, y_, dx_, dy_ = None, None, None, None
-first_click = True
-img = None
+x_, y_ = None, None
+isDrawing = False
+img, canvas = None, None
 current_file_path = None
+selections = []
+
+
+@dataclass
+class Selection:
+    x: float
+    y: float
+    x_: float
+    y_: float
+
+
+def showSelections():
+    for selection in selections:
+        cv2.rectangle(canvas, (selection.x_, selection.y_), (selection.x, selection.y), (255, 0, 0), 2)
+    cv2.imshow(current_file_path, canvas)
+
 
 # This functions is responsible for showing every <number> photo.
 # Please press any key to show next photo.
 def show_photo(list_of_paths, photos_skipped=5):
     file_index = 0
-    global img, current_file_path
+    global canvas, current_file_path
     for file_path in list_of_paths:
         if file_index == 0:
             current_file_path = file_path
-            img = cv2.imread(file_path)
-            cv2.imshow(file_path, img)
-            cv2.setMouseCallback(file_path, click_event)
-            cv2.waitKey(0)  # waits until a key is pressed
-            cv2.destroyAllWindows()  # destroys the window showing image
-        # print(f"current index number = {file_index} - file: {file_path}")
+            img = cv2.imread(current_file_path)
+            canvas = img.copy()
+            cv2.namedWindow(current_file_path)
+            cv2.setMouseCallback(current_file_path, click_event)
+            while (True):
+                cv2.imshow(current_file_path, canvas)
+                k = cv2.waitKey(0)
+                if k == 27:
+                    selections.clear()
+                    break
+                elif k == ord('d'):
+                    element_to_remove = len(selections) - 1
+                    del selections[element_to_remove]
+                    canvas = img.copy()
+                    showSelections()
+            cv2.destroyAllWindows()
         file_index = file_index + 1
         if file_index >= photos_skipped:
             file_index = 0
 
 
 def click_event(event, x, y, flags, param):
-    global x_, y_, dx_, dy_
-    global first_click
-    if not first_click:
-        if event == cv2.EVENT_LBUTTONUP:
-            dx_, dy_ = x, y
-            print(dx_, ' x ', dy_)
-            first_click = True
-            cv2.rectangle(img, (x_,y_), (dx_,dy_), (255, 0, 0), 2)
-            cv2.imshow(current_file_path, img)
-            tmp_bounding_box = BoundingBox(name=current_file_path, x=x, y=y, dx=dx_,dy=dy_)
-            bounding_box_list.append(tmp_bounding_box)
-            cv2.imwrite(current_file_path, img)
-            return
-
-    if first_click:
-        if event == cv2.EVENT_LBUTTONDOWN:
-            x_, y_ = x, y
-            print(x_, ' ', y_)
-            first_click = False
-            return
+    global x_, y_, isDrawing, current_file_path, canvas
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_, y_ = x, y
+        # print(x_, ' ', y_)
+        isDrawing = True
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if isDrawing:
+            imgCopy = canvas.copy()
+            cv2.rectangle(imgCopy, (x_, y_), (x, y), (255, 0, 0), 2)
+            cv2.imshow(current_file_path, imgCopy)
+    elif event == cv2.EVENT_LBUTTONUP:
+        selection = Selection(x_, y_, x, y)
+        selections.append(selection)
+        showSelections()
+        isDrawing = False
+        print(len(selections))
 
 
 def get_bounding_box_list():
